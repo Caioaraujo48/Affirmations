a6977193298968f06242a6150fb77e740fc6d074
Etec Profº Horácio Augusto da Silveira <caioquir@gmail.com>
2023-05-17 21:17:06 -0300

    Colocando Imagens e Frases(Eu acho)

--- app/src/main/java/com/ds2/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ds2/affirmations/adapter/ItemAdapter.kt
@@ -3,6 +3,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ds2.affirmations.R
@@ -12,8 +13,12 @@ class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
+    class ItemViewHolder(private val view: View):
+        RecyclerView.ViewHolder(view) {
+        val textView: TextView =
+            view.findViewById(R.id.item_title)
+        val imageView: ImageView =
+            view.findViewById(R.id.item_image)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
 // create a new view
@@ -24,6 +29,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
     override fun getItemCount() = dataset.size
 }
--- app/src/main/java/com/ds2/affirmations/data/Datasource.kt
+++ app/src/main/java/com/ds2/affirmations/data/Datasource.kt
@@ -6,12 +6,12 @@ import com.ds2.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation> (
-            Affirmation(R.string.Affirmation01),
-            Affirmation(R.string.Affirmation02),
-            Affirmation(R.string.Affirmation03),
-            Affirmation(R.string.Affirmation04),
-            Affirmation(R.string.Affirmation05),
-            Affirmation(R.string.Affirmation06)
+            Affirmation(R.string.Affirmation01, R.drawable.image1),
+            Affirmation(R.string.Affirmation02, R.drawable.image2),
+            Affirmation(R.string.Affirmation03, R.drawable.image3),
+            Affirmation(R.string.Affirmation04, R.drawable.image4),
+            Affirmation(R.string.Affirmation05, R.drawable.image5),
+            Affirmation(R.string.Affirmation06, R.drawable.image6)
         )
     }
 }
